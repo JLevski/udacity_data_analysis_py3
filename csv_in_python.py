d164c6782d4234e98359cdd635ba8a2789953a82
JLevski <jsalevin@gmail.com>
2017-10-22 14:59:29 +0200

    data from first week

--- csv_in_python.py
+++ csv_in_python.py
@@ -120,6 +120,34 @@ for enrollment in non_udacity_enrollments:
                 paid_students[account_key]):
             paid_students[account_key] = enrollment_date
 
-print(len(paid_students))
+# print(len(paid_students))
+
 
 # engagement records for paid students in their first week
+def within_one_week(join_date, engagement_date):
+    time_delta = engagement_date - join_date
+    return time_delta.days < 7
+
+
+def remove_free_trial_cancels(data):
+    new_data = []
+    for data_point in data:
+        if data_point['account_key'] in paid_students:
+            new_data.append(data_point)
+    return new_data
+
+
+paid_enrollments = remove_free_trial_cancels(non_udacity_enrollments)
+paid_engagement = remove_free_trial_cancels(non_udacity_engagement)
+paid_submissions = remove_free_trial_cancels(non_udacity_submissions)
+
+paid_engagement_in_first_week = []
+for engagement_record in paid_engagement:
+    account_key = engagement_record['account_key']
+    join_date = paid_students[account_key]
+    engagement_record_date = engagement_record['utc_date']
+
+    if within_one_week(join_date, engagement_record_date):
+        paid_engagement_in_first_week.append(engagement_record)
+
+print(len(paid_engagement_in_first_week))
